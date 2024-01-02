9dced1121f948db28f95d035715c0c7c99bca515
Mohammad Arham Imam <imamarham10@gmail.com>
2024-01-02 13:38:55 +0530

    bug fix at parallax.jsx

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import { useRef } from "react";
 import "./parallax.scss";
 import { motion, useScroll, useTransform} from "framer-motion";
 const Parallax = ({ type }) => {
